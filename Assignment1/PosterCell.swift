d690e01f6d256d4bacd76f72b3c68ea525ab99fd
LinuxPlus <linuxplus@LinuxPluss-Mac.local>
2018-09-20 23:04:21 -0700

    part 2 nearly done

---
+++ Assignment1/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Assignment1
+//
+//  Created by LinuxPlus on 9/20/18.
+//  Copyright © 2018 LinuxPlus. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
